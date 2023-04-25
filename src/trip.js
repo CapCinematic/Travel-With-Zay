cdf88d023a5b82c2916073c502d0c8aed7a9f6ff
Isaiah Richardson <capcinematic@gmail.com>
2023-04-24 21:54:10 -0600

    display dynamic traveler info to the DOM

--- src/trip.js
+++ src/trip.js
@@ -1,22 +1,26 @@
 
 import { getTraveler, getTrip, getDestination } from "./data.js/getData"
+import Destination from "./destination"
 
 class Trip {
   constructor(tripObj){
     this.date = tripObj.date
     this.destinationID = tripObj.destinationID
+    this.destination = undefined
     this.duration = tripObj.duration
     this.id = tripObj.id
     this.status = tripObj.status
     this.suggestedActivities = tripObj.suggestedActivities
     this.travelers = tripObj.travelers
     this.userID = tripObj.userID
+    
   }
 
-  getTripsByTraveler(traveler){
-    return this.trips.find(trip => trip.id === traveler.id)
+  findDestination(destinations){
+    const match = destinations.find((destination) => destination.id === this.destinationID)
+    const destination = new Destination(match)
+    this.destination = destination
   }
-
   
 }
 
