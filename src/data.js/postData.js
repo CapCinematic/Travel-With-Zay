c4d7732687f808845d2d796d56c35dbb6990c3e7
Isaiah Richardson <capcinematic@gmail.com>
2023-04-26 08:31:49 -0600

    re-testing of classes

--- src/data.js/postData.js
+++ src/data.js/postData.js
@@ -17,7 +17,6 @@ function postTrip(userID, destinationID, travelers, date, duration, status, sugg
   })
     .then((response) => response.json())
     .then((data) => {
-      // create new Trip object and add it to currentTravelerTrips array
       console.log(data);
     })
     .catch((error) => console.log(error));
