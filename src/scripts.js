02995c0c6f15a7399f775086d190446178de3560
Isaiah Richardson <capcinematic@gmail.com>
2023-04-25 22:03:56 -0600

    add photos

--- src/scripts.js
+++ src/scripts.js
@@ -2,60 +2,64 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
-
+import "./css/styles.css";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import {fetchAll, fetchData} from './data.js/getData';
-import Traveler from './traveler';
-import Trip from './trip';
-import Destination from './destination';
-import postTrip from './data.js/postData';
+import "./images/turing-logo.png";
+import { fetchAll, fetchData } from "./data.js/getData";
+import Traveler from "./traveler";
+import Trip from "./trip";
+import Destination from "./destination";
+import postTrip from "./data.js/postData";
 
 let currentTraveler;
 let currentTravelerTrips;
-const logInInput = document.querySelector('.log-in')
-const displayInBody = document.querySelector('.body')
-const planTripButton = document.querySelector('.plan-trip-button')
-const homeButton = document.querySelector('.home-button')
-const travelCalendarValue = document.querySelector('.travel-calendar')
-const submitTravelButton = document.querySelector('.submit-travel-data')
-const datePickerValue = document.querySelector('.choose-travel-date')
-const numTravelers = document.querySelector('.num-travelers')
-const durationNum = document.querySelector('.duration-num')
-const selectDestination = document.querySelector('.select-destination')
-const makeTripForm = document.querySelector('.make-trip-form')
-const travelerSection = document.querySelector('.traveler-data-section')
-const tripsDisplay = document.querySelector('.table')
-window.addEventListener('load', travelerLogin)
-submitTravelButton.addEventListener('click', makeTrip)
-planTripButton.addEventListener('click', viewForm)
+const logInInput = document.querySelector(".log-in");
+const displayInBody = document.querySelector(".body");
+const planTripButton = document.querySelector(".plan-trip-button");
+const homeButton = document.querySelector(".home-button");
+const travelCalendarValue = document.querySelector(".travel-calendar");
+const submitTravelButton = document.querySelector(".submit-travel-data");
+const datePickerValue = document.querySelector(".choose-travel-date");
+const numTravelers = document.querySelector(".num-travelers");
+const durationNum = document.querySelector(".duration-num");
+const selectDestination = document.querySelector(".select-destination");
+const makeTripForm = document.querySelector(".make-trip-form");
+const travelerSection = document.querySelector(".traveler-data-section");
+const tripsDisplay = document.querySelector(".table");
+const displayPhoto =document.querySelector(".image-section")
+window.addEventListener("load", travelerLogin);
+submitTravelButton.addEventListener("click", makeTrip);
+planTripButton.addEventListener("click", viewForm);
+homeButton.addEventListener('click', getInspired)
 
-function getData(e){
-  e.preventDefault()
-  homeButton.classList.remove('hidden')
-  planTripButton.classList.remove('hidden')
-  const nameInput = document.querySelector('.name-input')
-  const passwordInput = document.querySelector('.password-input')
-  const travelersId = nameInput.value.split('traveler')[1]
+function getData(e) {
+  e.preventDefault();
+  homeButton.classList.remove("hidden");
+  planTripButton.classList.remove("hidden");
+  const nameInput = document.querySelector(".name-input");
+  const passwordInput = document.querySelector(".password-input");
+  const travelersId = nameInput.value.split("traveler")[1];
   fetchAll(travelersId).then((travelerData) => {
-    travelerData[0].travelers.map((traveler) => new Traveler(traveler))
-    travelerData[2].trips.filter((trip) => trip.userID === 1).map((trip) => new Trip(trip))
-    currentTraveler = new Traveler(travelerData[1])
-    currentTravelerTrips =  travelerData[2].trips.filter((trip) => trip.userID === currentTraveler.id).map((trip) => {
-      const newTrip = new Trip(trip, trip.travelers, trip.duration)
-      console.log('scripLog',trip.duration)
-      newTrip.findDestination(travelerData[3].destinations)
-      return newTrip
-    })
-    displayTravelerData()
-    console.log(currentTraveler)
-  })
+    travelerData[0].travelers.map((traveler) => new Traveler(traveler));
+    travelerData[2].trips
+      .filter((trip) => trip.userID === 1)
+      .map((trip) => new Trip(trip));
+    currentTraveler = new Traveler(travelerData[1]);
+    currentTravelerTrips = travelerData[2].trips
+      .filter((trip) => trip.userID === currentTraveler.id)
+      .map((trip) => {
+        const newTrip = new Trip(trip, trip.travelers, trip.duration);
+        console.log("scripLog", trip.duration);
+        newTrip.findDestination(travelerData[3].destinations);
+        return newTrip;
+      });
+    displayTravelerData();
+    console.log(currentTraveler);
+  });
 }
 
-
-function travelerLogin (e){
-  e.preventDefault()
+function travelerLogin(e) {
+  e.preventDefault();
   logInInput.innerHTML = `
   <legend>Information:</legend>
        Full Name:<br>
@@ -63,14 +67,14 @@ function travelerLogin (e){
        Password:<br>
        <input class="password-input" type="text" name="password" value="" placeholder="Password"><br>
        <button class="view-traveler-data">Log-In</button>
-  ` 
-  const logInButton = document.querySelector('.view-traveler-data')
-  logInButton.addEventListener('click', getData)
+  `;
+  const logInButton = document.querySelector(".view-traveler-data");
+  logInButton.addEventListener("click", getData);
 }
 
-function displayTravelerData (){
-  homeButton.classList.remove('hidden')
-  planTripButton.classList.remove('hidden')
+function displayTravelerData() {
+  homeButton.classList.remove("hidden");
+  planTripButton.classList.remove("hidden");
   travelerSection.innerHTML = `
   <table class="table" view>
       <colgroup>
@@ -87,15 +91,15 @@ function displayTravelerData (){
       </tr>
       </colgroup>
     </table> 
-  `
-  displayTrips()
+  `;
+  displayTrips();
 }
 
-function displayTrips(){
-  const table = document.querySelector('.table')
-  logInInput.classList.add('hidden')
-  currentTravelerTrips.forEach(trip => {
-    table.innerHTML +=` 
+function displayTrips() {
+  const table = document.querySelector(".table");
+  logInInput.classList.add("hidden");
+  currentTravelerTrips.forEach((trip) => {
+    table.innerHTML += ` 
     <tr>
         <td>${trip.date}</td>
         <td>${trip.status}</td>
@@ -105,27 +109,44 @@ function displayTrips(){
         <td>${trip.destination.estimatedLodgingCostPerDay}</td>
         <td>${trip.totalCost}</td>
     </tr>
-  `
-  })
- 
-}
-function viewForm (){
-  makeTripForm.classList.remove('hidden')
-  logInInput.classList.add('hidden')
+  `;
+  });
 }
 
-function makeTrip(e){
-  e.preventDefault()
-  const [year,month,day] = datePickerValue.value.split('-')
-  makeTripForm.classList.remove('hidden')
-  const userId = currentTraveler.id
-  const destinationId = Number(selectDestination.value)
-  const travelers = numTravelers.value
-  const date = `${year}/${month}/${day}`
-  console.log('date', date)
-  const duration = durationNum.value
-  const status = 'pending'
-  const suggestedActivities = []
-  console.log('makeTripCurT',currentTraveler.id)
-  postTrip(userId, destinationId, travelers, date, duration,status, suggestedActivities)
+function viewForm() {
+  makeTripForm.classList.remove("hidden");
+  logInInput.classList.add("hidden");
 }
+
+function makeTrip(e) {
+  e.preventDefault();
+  const [year, month, day] = datePickerValue.value.split("-");
+  makeTripForm.classList.remove("hidden");
+  const userId = currentTraveler.id;
+  const destinationId = Number(selectDestination.value);
+  const travelers = numTravelers.value;
+  const date = `${year}/${month}/${day}`;
+  console.log("date", date);
+  const duration = durationNum.value;
+  const status = "pending";
+  const suggestedActivities = [];
+  console.log("makeTripCurT", currentTraveler.id);
+  postTrip(
+    userId,
+    destinationId,
+    travelers,
+    date,
+    duration,
+    status,
+    suggestedActivities
+  );
+}
+
+function getInspired(){
+ displayPhoto.classList.remove('hidden')
+ travelerSection.classList.add('hidden')
+}
+
+function getRandomIndex(array){
+  return Math.floor(Math.random() * array.length)
+};
